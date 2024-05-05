0eb05f31c4815acede0d6c76e010ae673726f3b0
Ron Sims II <ronsims2@gmail.com>
2024-05-05 15:25:16 -0400

    Added code to create the config dir.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 mod setup;
 
 use std::env;
-use crate::setup::create_config;
+use std::path::Path;
+use crate::setup::{check_for_config, create_crusty_dir, get_home_dir, init_crusty_db};
 
 fn main() {
     // check for a crusty home directory, if it doesn't exist show setup prompt
-    create_config()
+    let home_dir = get_home_dir();
+    match check_for_config(&home_dir) {
+        None => {
+            println!("This is the create config path.");
+            create_crusty_dir();
+            init_crusty_db();
+        }
+        Some(conf_path) => {
+            println!("This is the update config path: {:?}", conf_path)
+        }
+    };
 }
--- src/setup.rs
+++ src/setup.rs
@@ -1,27 +1,92 @@
-use std::env;
+use std::{env, fs};
 use std::env::VarError;
-use std::path::Path;
+use std::ffi::OsStr;
+use std::path::{Path, PathBuf};
+use std::process::exit;
+use std::string::ToString;
+use clap::builder::Str;
 use clap::Parser;
 
 
-pub(crate) fn create_config() {
+fn get_win_home_drive() -> String {
+    let win_home_drive = match env::var("HOMEDRIVE") {
+        Ok(val) => {
+            val.to_string()
+        }
+        Err(_) => {
+            println!("Could not determine Windows user drive.");
+            exit(501)
+        }
+    };
+
+    win_home_drive
+}
+
+pub(crate) fn get_home_dir() -> String {
+    let windows_os = "windows";
     // assume we are in a *nix env but update home path for windows if detected
     let os_fam = env::consts::FAMILY;
-     let home_key = if &os_fam.eq("windows") {"%HOMEPATH%"} else {"HOME"};
-     let mut user_home = match env::var(home_key) {
-         Ok(val) => {
-             println!("The users is on a: {} system, and their home dir is at: {}", os_fam, val);
-             val.to_string()
-         },
-         Err(err) => {
-             println!("Error: {}", err);
-             "".to_string()
-         }
-     };
+    let home_key = if os_fam.eq(windows_os) { "HOMEPATH" } else { "HOME" };
+    let user_home = match env::var(home_key) {
+        Ok(val) => {
+            if os_fam.eq(windows_os) {
+                let whd = get_win_home_drive();
+                format!("{}/{}", whd, &val).to_string()
+            } else {
+                val.to_string()
+            }
+        }
+        Err(_) => {
+            println!("Could not determine home path during config.");
+            exit(502)
+        }
+    };
 
+    user_home
+}
 
-    println!("The home directory is at: {}", user_home)
-    // look for config first in home dir
-    // let home_loc = homedir::get_home();
-    // let conf_loc = Path::new().is_file();
+pub(crate) fn get_crusty_dir() -> PathBuf {
+    let user_home = get_home_dir();
+    let config_loc = format!("{}/.crusty", &user_home);
+    Path::new(&config_loc).to_path_buf()
+}
+
+pub(crate) fn check_for_config(home_dir: &String) -> Option<PathBuf> {
+    let config_path = get_crusty_dir();
+
+    if config_path.exists() {
+        return Some(config_path)
+    }
+    None
+}
+
+pub(crate) fn create_crusty_dir() {
+    let config_path = get_crusty_dir();
+    match fs::create_dir(&config_path) {
+        Ok(_) => {
+            println!("Created cRusty config at: {:?}", config_path);
+        }
+        Err(_) => {
+            println!("Could not create cRusty config directory.");
+            exit(503)
+        }
+    }
+}
+
+pub(crate) fn get_crusty_db_path() -> PathBuf {
+    let config_path = get_crusty_dir();
+    config_path.join("crusty.db")
+}
+
+pub(crate) fn init_crusty_db() {
+    let db_path = get_crusty_db_path();
+    let db_created = fs::File::create(db_path.as_path());
+    match db_created {
+        Ok(_) => {
+            println!("cRusty DB created.")
+        }
+        Err(_) => {
+            println!("Could not create cRusty DB.")
+        }
+    }
 }
