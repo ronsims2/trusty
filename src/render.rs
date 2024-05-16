782e5112a932e3831e82efd5e4a66366366f1c2a
Ron Sims II <ronsims2@gmail.com>
2024-05-16 14:00:45 -0400

    Fixed formatting and added a delimeter line.

--- src/render.rs
+++ src/render.rs
@@ -5,6 +5,7 @@ use crate::sql::NoteSummary;
 pub(crate) fn print_note_summary(note: NoteSummary) {
     let stdout = io::stdout();
     let mut handle = stdout.lock();
-    let text = format!("{:width$} | {:title_width$} | {}", note.id, note.title, note.updated, width = 9, title_width = 53);
+    let text = format!("{:width$} | {:title_width$} | {}", note.id, note.title, note.updated, width = 9, title_width = 45);
     writeln!(&mut handle, "{}", text).unwrap();
+    writeln!(&mut handle, "{}", "-".repeat(80)).unwrap();
 }
