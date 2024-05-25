5c3730808283f9ae822a5aacd4ca3cbef78d1bb2
Ron Sims II <ronsims2@gmail.com>
2024-05-25 14:24:40 -0400

    Removed cruft.

--- src/utils.rs
+++ src/utils.rs
@@ -4,8 +4,6 @@ pub(crate) fn slice_text(start: usize, stop: usize, text: &str) -> String {
     let chars = text.graphemes(true).collect::<Vec<&str>>();
     let char_count = chars.iter().count();
 
-    println!("Char count: {}", char_count);
-
     return if stop - start < char_count {
         let sample = &chars[start..stop];
         sample.join("").to_string()
