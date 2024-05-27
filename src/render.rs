a55255a991d6259e33e3887af67cf9451f524ceb
Ron Sims II <ronsims2@gmail.com>
2024-05-27 14:08:36 -0400

    Fixed bug where title would mess up menu display.

--- src/render.rs
+++ src/render.rs
@@ -1,12 +1,14 @@
 use std::io;
 use std::io::Write;
 use crate::sql::{NoteSummary, SimpleNoteView};
+use crate::utils::slice_text;
 
 pub(crate) fn print_note_summary(note: NoteSummary) {
     // @todo refactor to use cr_print
     let stdout = io::stdout();
     let mut handle = stdout.lock();
-    let text = format!("{:width$} | {:title_width$} | {}", note.id, note.title, note.updated, width = 9, title_width = 45);
+    let title = slice_text(0,45, &note.title);
+    let text = format!("{:width$} | {:title_width$} | {}", note.id, title, note.updated, width = 9, title_width = 45);
     writeln!(&mut handle, "{}", text).unwrap();
     writeln!(&mut handle, "{}+{}+{}", "-".repeat(10), "-".repeat(47), "-".repeat(21)).unwrap();
 }
@@ -18,7 +20,7 @@ pub(crate) fn cr_println(text: String) {
 }
 
 pub(crate) fn print_simple_note(note: SimpleNoteView) {
-    // @todo Right now it is bette rto not render the title so that using things liked saved data are easier
+    // @todo Right now it is better to not render the title so that using things liked saved data are easier
     // cr_println(note.title);
     // cr_println(format!("{}", "_".repeat(80)));
     cr_println(note.body);
