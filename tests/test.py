a545e189b9f1d91cc6b73209e5b7928d528b3ec7
Ron Sims II <ronsims2@gmail.com>
2024-07-06 22:51:47 -0400

    Have PoC for python E2E tests done.

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,8 @@
 # set the TRUSTY_HOME so that we can use a test db
-from os import environ
+from os import environ, popen
 import subprocess
 import tempfile
 import io
-from contextlib import redirect_stdout,redirect_stderr
 
 trusty_home = 'TRUSTY_HOME'
 temp_dir = tempfile.gettempdir()
@@ -11,11 +10,13 @@ environ[trusty_home] = temp_dir
 
 print(f'trusty home dir: {environ.get(trusty_home)}')
 
-std_o = io.StringIO()
-with redirect_stdout(std_o):
-    subprocess.run(['tru', '--help'])
 
-print(f'STD OUT: {std_o.getvalue()}')
+output = popen('tru --help')
+
+for line in output.readlines():
+    print(f'STD OUT: 🐍{line}🐍')
+
+
 
 
 
