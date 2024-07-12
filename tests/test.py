4fc18527803fbd8f0b32e77ceb342ddf10ee357a
Ron Sims II <ronsims2@gmail.com>
2024-07-11 22:58:39 -0400

    Did a small refactor of the tests

--- tests/test.py
+++ tests/test.py
@@ -14,22 +14,30 @@ TEST_PASSWORD = '1234'
 TEST_RECOVERY_CODE = '7e85e714-60fd-4e8c-a58e-73674314101c'
 
 
-def get_menu_output():
+def get_menu_output(tru):
     return Popen(f'{tru}', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
 
 
-def get_note_by_id(id):
+def get_note_by_id(tru, id):
     return Popen(f'{tru} -f {id}', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
 
 
-def get_encrypted_note_by_id(id, pwd):
+def get_encrypted_note_by_id(tru, note_id, pwd):
     # buf = BytesIO()
-    proc = pexpect.spawn(f'{tru} -f {id}')
+    proc = pexpect.spawn(f'{tru} -f {note_id}')
     # proc.logfile = buf
     proc.expect('Enter password:')
     proc.sendline(pwd)
 
     return proc.read().decode()
+
+
+def hard_delete_by_id(tru, id):
+    Popen(f'{tru} -F {id}', shell=True, stderr=PIPE, stdout=PIPE).stdout.read().decode()
+    return Popen(f'{tru} -f {id}',
+                 shell=True,
+                 stderr=PIPE,
+                 stdout=PIPE).stderr.read().decode()
 
 
 curr_dir = getcwd()
@@ -59,7 +67,7 @@ print(f'Database location: {trusty_db_path}')
 
 environ[trusty_home] = trusty_home_dir
 # the path to the built executable
-tru = path.join(getcwd(), 'target', 'debug', 'tru')
+trusty = path.join(getcwd(), 'target', 'debug', 'tru')
 
 print(f'{trusty_home} env var set to: {environ.get(trusty_home)}')
 
@@ -71,15 +79,15 @@ except Exception as e:
     print('Could not build project')
 
 # Test the help command
-help_output = Popen(f'{tru} --help', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
+help_output = Popen(f'{trusty} --help', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
 assert 'tRusty: a command line notes app  🦀📝' in help_output
 print('✅ --help test passed')
 
 # Test tRusty --list
 control_default_output = 'Get Started with tRusty'
-default_output = Popen(f'{tru}', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
+default_output = Popen(f'{trusty}', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
 assert control_default_output in default_output
-list_output = Popen(f'{tru}', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
+list_output = Popen(f'{trusty}', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
 assert control_default_output in list_output
 print('✅ --list test passed')
 
@@ -98,9 +106,9 @@ Nulla tincidunt, sem vitae luctus dignissim, 🥷 lacus nibh consequat erat, nec
 '''
 
 # Test adding a note
-Popen(f'{tru} -t "{control_title}" -n "{control_body}"', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
-menu_output = get_menu_output()
-note_output = get_note_by_id(2)
+Popen(f'{trusty} -t "{control_title}" -n "{control_body}"', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
+menu_output = get_menu_output(trusty)
+note_output = get_note_by_id(trusty, 2)
 assert control_body in note_output
 assert '🤣Foobar Barbaz 🥷 Bazbez Lorem ipsum dolor s' in menu_output
 print('✅ -n -t test passed')
@@ -109,8 +117,8 @@ print('✅ -n -t test passed')
 control_quick_note = '''🥷🤣🐶Nulla tincidunt, sem vitae luctus dignissim, 🥷 
 lacus nibh consequat erat, nec tristique ipsum dui et ex.\r\n
 Lorem  🤣 ipsum dolor sit amet, consectetur adipiscing elit.'''
-Popen(f'{tru} -q "{control_quick_note}"', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
-menu_output = get_menu_output()
+Popen(f'{trusty} -q "{control_quick_note}"', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
+menu_output = get_menu_output(trusty)
 assert '🥷🤣🐶Nulla tincidunt, sem vitae luctus digni' in menu_output
 print('✅ -q test passed')
 
@@ -118,17 +126,17 @@ print('✅ -q test passed')
 piped_note = '''🥷Nulla tincidunt, sem vitae luctus dignissim, 🥷 
 lacus nibh consequat erat, 🤣🐶nec tristique ipsum dui et ex.
 Lorem  🤣 ipsum dolor sit amet, consectetur adipiscing elit.🥷'''
-Popen(f'echo "{piped_note}" | {tru} -i', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
-menu_output = get_menu_output()
+Popen(f'echo "{piped_note}" | {trusty} -i', shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
+menu_output = get_menu_output(trusty)
 assert 'Untitled' in menu_output
-note_output = get_note_by_id(4)
+note_output = get_note_by_id(trusty, 4)
 assert piped_note.strip() == note_output.strip()
 print('✅ -i test passed')
 # test pipe dnote with title
 piped_title = "🥷Bar Foo 🥷"
-Popen(f'echo "{piped_note}" | {tru} -i -t "{piped_title}"',
+Popen(f'echo "{piped_note}" | {trusty} -i -t "{piped_title}"',
       shell=True, stderr=None, stdout=PIPE).stdout.read().decode()
-menu_output = get_menu_output()
+menu_output = get_menu_output(trusty)
 assert piped_title in menu_output
 print('✅ -i -t test passed')
 
@@ -136,53 +144,52 @@ control_encrypted_title = '🔒 ENCRYPTED'
 # Add an encrypted note with title
 encrypted_note_title = '🥷🏽❤️🗡️'
 encrypted_note_body = 'Ninjas loves swords'
-child = pexpect.spawn(f'{tru} -t "{encrypted_note_title}" -n "{encrypted_note_body}" -E')
+child = pexpect.spawn(f'{trusty} -t "{encrypted_note_title}" -n "{encrypted_note_body}" -E')
 child.expect('Enter password:')
 child.sendline(TEST_PASSWORD)
-menu_output = get_menu_output()
+menu_output = get_menu_output(trusty)
 assert control_encrypted_title in menu_output
-assert encrypted_note_body in get_encrypted_note_by_id(6, TEST_PASSWORD)
+assert encrypted_note_body in get_encrypted_note_by_id(trusty,6, TEST_PASSWORD)
 print('✅ -t -n -E test passed')
 
 # Add an encrypted quicknote
 encrypted_quick_note = '🐶🐶🐶 Foobar Dog 🐶🐶🐶'
-child = pexpect.spawn(f'{tru} -q "{encrypted_quick_note}" -E')
+child = pexpect.spawn(f'{trusty} -q "{encrypted_quick_note}" -E')
 child.expect('Enter password:')
 child.sendline(TEST_PASSWORD)
-menu_output = get_menu_output()
+menu_output = get_menu_output(trusty)
 assert control_encrypted_title in menu_output
-assert encrypted_quick_note in get_encrypted_note_by_id(7, TEST_PASSWORD)
+assert encrypted_quick_note in get_encrypted_note_by_id(trusty, 7, TEST_PASSWORD)
 print('✅ -q -E test passed')
 
 # Add an encrypted piped note
 encrypted_piped_note = '👀 peek-a-boo'
-child = pexpect.spawn(f'{tru} -i -E')
+child = pexpect.spawn(f'{trusty} -i -E')
 child.sendline(encrypted_piped_note)
 child.sendeof()
 child.expect('Enter password:')
 child.sendline(TEST_PASSWORD)
-menu_output = get_menu_output()
+menu_output = get_menu_output(trusty)
 assert control_encrypted_title in menu_output
-assert encrypted_piped_note in get_encrypted_note_by_id(8, TEST_PASSWORD)
+assert encrypted_piped_note in get_encrypted_note_by_id(trusty, 8, TEST_PASSWORD)
 print('✅ -i -E test')
 
 # Add an encrypted piped note with title
 encrypted_piped_id = 9
 encrypted_piped_title = '👀👀👀 LOOKEY HERE'
-child = pexpect.spawn(f'{tru} -i -t "{encrypted_piped_title}" -E')
+child = pexpect.spawn(f'{trusty} -i -t "{encrypted_piped_title}" -E')
 child.sendline(encrypted_piped_note)
 child.sendeof()
 child.expect('Enter password:')
 child.sendline(TEST_PASSWORD)
-menu_output = get_menu_output()
+menu_output = get_menu_output(trusty)
 assert control_encrypted_title in menu_output
-assert encrypted_piped_note in get_encrypted_note_by_id(encrypted_piped_id, TEST_PASSWORD)
+assert encrypted_piped_note in get_encrypted_note_by_id(trusty, encrypted_piped_id, TEST_PASSWORD)
 print('✅ -i -E test')
 
 
 # Hard delete an encrypted note
-Popen(f'{tru} -F {encrypted_piped_id}', shell=True, stderr=PIPE, stdout=PIPE).stdout.read().decode()
-result = Popen(f'{tru} -f {encrypted_piped_id}', shell=True, stderr=PIPE, stdout=PIPE).stderr.read().decode()
+result = hard_delete_by_id(trusty, 9)
 assert 'Could not find note for id: 9' == result.strip()
 print('✅ -D test')
 
