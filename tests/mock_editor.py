74ac8b5d797be560e7e5a76ba3dad6cc1b17f785
Ron Sims II <ronsims2@gmail.com>
2024-07-23 21:38:50 -0400

    Fixed a typo in a comment

--- tests/mock_editor.py
+++ tests/mock_editor.py
@@ -3,7 +3,7 @@ import sys
 
 
 temp_file = sys.argv[1]
-# thsi just test to see if the internal api passes a temp file.  This implies that the app code is correct.
+# this just test to see if the internal api passes a temp file.  This implies that the app code is correct.
 if os.path.isfile(temp_file):
     exit(0)
 else:
